c3b396e9c8b277429cb7b479e1dfea0f85b87c30
Connie Leung <cleung334@gmail.com>
2021-04-06 17:45:26 +0800

    refactor:  app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
   constructor(titleService: Title) {
