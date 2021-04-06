321124ff7026f327cf54411eb65a98c0cfb51c42
Connie Leung <cleung334@gmail.com>
2021-04-06 16:38:00 +0800

    fix: course card
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -53,25 +53,7 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
         Lesson: {
           fields: {
             paginatedSentences: {
-              // merge(existing: any, incoming: any, { mergeObjects, readField }): any {
               merge(existing: any, incoming: any, { mergeObjects }): any {
-                // Obtain a Set of all existing task IDs.
-                // const existingIdSet = new Set(
-                //   (existing?.sentences || []).map((paginatedItem: any) => readField('id', paginatedItem)));
-                // // Remove incoming tasks already present in the existing data.
-                // const incomingSentences = incoming.sentences.filter(
-                //    (paginatedItem: any) => !existingIdSet.has(readField('id', paginatedItem)));
-
-                // // return mergeObjects(existing, incoming);
-                // if (existing && incoming) {
-                //   const existingCursor = (existing.cursor || -1) as number;
-                //   const incomingingCursor = (incoming.cursor || -1) as number;
-                //   return {
-                //     cursor: Math.max(existingCursor, incomingingCursor),
-                //     sentences: [...existing.sentences, ...incomingSentences]
-                //   };
-                // }
-                // return incoming;
                 return mergeObjects(existing, incoming);
               },
             }
