c7b83a018ce97ba441ec601aeddf3918c43f07e1
Connie Leung <cleung334@gmail.com>
2021-04-16 04:03:29 +0800

    chore:  remove unused import

--- src/app/course/lessons/lessons.component.ts
+++ src/app/course/lessons/lessons.component.ts
@@ -1,4 +1,3 @@
-import { PaginatedItems } from './../../generated/graphql';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of } from 'rxjs';
